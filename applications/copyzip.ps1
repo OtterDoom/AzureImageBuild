ca932e17e31f2bdc1bb5b880f28b5f2e91f57087
otterdoom <otterdoom@outlook.com>
2023-01-17 16:29:14 -0500

    update

--- applications/copyzip.ps1
+++
@@ -1,4 +0,0 @@
-write-host "$(Get-Date) AIB Customization: Copying Zip file"
-Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Copying Zip file"
-Copy-Item -Path \\gsazfilescorp.file.core.windows.net\azureimagebuilderfiles\applications\lookatme.zip -Destination c:\temp
-Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Copying Zip file Complete"
