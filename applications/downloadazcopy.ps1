efef30ff01bd7d00300a4f9824f0b3fe5c613636
otterdoom <otterdoom@outlook.com>
2023-01-17 21:45:16 -0500

    updates

--- applications/downloadazcopy.ps1
+++ applications/downloadazcopy.ps1
@@ -10,11 +10,12 @@ Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customizat
 Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Download package name is $apppkg"
 New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
 #Download command
-(New-Object Net.WebClient).DownloadFile("$appURI", "$outputPath")
-Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) Download of $apppkg complete"
 Write-Host "$(Get-Date) AIB Customization: Starting Download of $appname"
 Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Starting download of $appname"
+(New-Object Net.WebClient).DownloadFile("$appURI", "$outputPath")
+Write-Host "$(Get-Date) Download of $apppkg complete"
+Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) Download of $apppkg complete"
 # Set your download switches and arguments
 Expand-Archive $outputPath -DestinationPath $LocalPath
-Write-host "$(Get-Date) AIB Customization: Finished download of $appname"
-Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Finished download of $appname"
+Write-host "$(Get-Date) AIB Customization: Finished unzip of $appname"
+Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Finished unzip of $appname"
