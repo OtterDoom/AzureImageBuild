4061227c537b240b70d80aa28a31934e601786d5
otterdoom <otterdoom@outlook.com>
2023-01-21 15:01:53 -0500

    update

--- applications/downloadagents.ps1
+++ applications/downloadagents.ps1
@@ -1,5 +1,5 @@
 $appName = "agents"
-$appURI = "https://saaibappseus.blob.core.windows.net/applications/agents.zip"
+$appURI = "https://saazureimagebuildereu2.blob.core.windows.net/applications/agents.zip"
 $apppkg = "agents.zip"
 $drive = "C:\temp"
 $LocalPath = $drive + '\' + $appName
