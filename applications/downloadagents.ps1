113995c2bbdbe672f23b16ab54f69e20570ee83c
otterdoom <otterdoom@outlook.com>
2023-01-25 15:00:48 -0500

    update

--- applications/downloadagents.ps1
+++ applications/downloadagents.ps1
@@ -13,7 +13,7 @@ New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyCo
 #login as AZCopy using managed identity
 $env:AZCOPY_AUTO_LOGIN_TYPE="MSI"
 $env:AZCOPY_MSI_OBJECT_ID="2959ba51-134a-469d-8966-de1f513963e2"
-C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity
+# C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity
 # C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity --identity-object-id "2959ba51-134a-469d-8966-de1f513963e2"
 
 #Download command
