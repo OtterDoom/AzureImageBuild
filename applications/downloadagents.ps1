9f32fd6fc5d46d86da9dac0398ba917c04dc1106
otterdoom <otterdoom@outlook.com>
2023-01-25 12:59:21 -0500

    update

--- applications/downloadagents.ps1
+++ applications/downloadagents.ps1
@@ -11,7 +11,10 @@ Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customizat
 New-Item -Path $drive -Name $appName -ItemType Directory -ErrorAction SilentlyContinue
 
 #login as AZCopy using managed identity
-C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity --identity-object-id "2959ba51-134a-469d-8966-de1f513963e2"
+$env:AZCOPY_AUTO_LOGIN_TYPE=MSI
+$env:AZCOPY_MSI_OBJECT_ID=2959ba51-134a-469d-8966-de1f513963e2
+C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity
+# C:\temp\apps\azcopy\azcopy_windows\azcopy.exe login --identity --identity-object-id "2959ba51-134a-469d-8966-de1f513963e2"
 
 #Download command
 Write-Host "$(Get-Date) AIB Customization: Starting Download of $appname"
