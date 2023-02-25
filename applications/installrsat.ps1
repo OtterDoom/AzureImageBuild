99056c56a1d87bcd169568453698e60a4f67c32d
otterdoom <otterdoom@outlook.com>
2023-02-25 14:33:09 -0500

    update

--- applications/installrsat.ps1
+++ applications/installrsat.ps1
@@ -4,6 +4,6 @@ Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customizat
 Write-Host "$(Get-Date) AIB Customization: Starting Install of $appname"
 Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Starting Install of $appname"
 # Set your install switches and arguments
-Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability –Online
+Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
 Write-host "$(Get-Date) AIB Customization: Finished Install of $appname"
 Add-Content -LiteralPath C:\New-SessionHostImage.log "$(Get-Date) AIB Customization: Finished Install of $appname"
